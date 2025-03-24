6e12b889ee1b2719bed4d971e399dd3ac75466dd
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-03-24 14:41:36 -0700

    Modified function_d in a silly way.

--- function_d.py
+++ function_d.py
@@ -1,9 +1,8 @@
 def max_value(numbers):
-    """ This function returns the largest number
-        in the list.
-    """
-    pass
-
+    value = 0
+    for number in numbers:
+        value +=1
+    return value
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
